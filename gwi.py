9909a32a0568e66bbb1784aa145a8eab239dc6cf
Tristram Walsh <tristramwalsh@gmail.com>
2022-11-14 13:50:34 +0000

    Add ensemble-based uncertainty for ERF and HadCRUT.

--- gwi.py
+++ gwi.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
 
 # DEFINE AR5-IR MODEL
 # Code copied from warming contributions
@@ -124,25 +125,27 @@ def a_params(gas):
         return a_n2o
 
 
-def regress_single_gwi(forc_All, temp_Obs, params):
+def regress_single_gwi(forc_All, temp_Obs, params, offset=False):
     """Regress GWI timeseries for single given ERF and Observed Temperature."""
     
     temp_All = forc_All.copy()
-    vars = list(forc_All)[1:]
+    vars = list(forc_All)
     for var in vars:
         # Calculate the temperature from ERF
         _forc = forc_All[var]
         _temp = FTmod(forc_All.shape[0], params) @ _forc
         temp_All[var] = _temp
         # Substract preindustrial baseline
-        _ofst = temp_All.loc[(temp_All['Year'] >= start_pi) &
-                             (df_forc['Year'] <= end_pi),
+        _ofst = temp_All.loc[(temp_All.index >= start_pi) &
+                             (temp_All.index <= end_pi),
                              var
                              ].mean()
-        temp_All[var] = temp_All.loc[(temp_All['Year'] >= start_yr) &
-                                     (df_forc['Year'] <= end_yr),
-                                     var
-                                     ] - _ofst
+        temp_All[var] -= _ofst
+        
+    temp_All = temp_All.loc[(temp_All.index >= start_yr) &
+                            (temp_All.index <= end_yr)]
+    
+    # print(temp_All.head())
         
     # Calculate regression coefficients
     # The following code below is equivalent to the (more easily readable)
@@ -154,24 +157,28 @@ def regress_single_gwi(forc_All, temp_Obs, params):
     # Then we regain (outside this function function for now)
     # temp_TOT = coef_GHG * temp_GHG + coef_Aer * temp_Aer + coef_Nat * temp_Nat + coef_Cst
 
-    temp_All['ones'] = np.ones(temp_All.shape[0])
-    temp_Mod = np.array(temp_All)[:, 1:]
-    coef_Reg = np.linalg.lstsq(temp_Mod, temp_Obs)[0]
-    for i in range(temp_Mod.shape[1]):  # ie number of variables
-        temp_Mod[:, i] *= coef_Reg[i]
-    
-    return temp_Mod
-    
+    if offset:
+        temp_All['ones'] = np.ones(temp_All.shape[0])
+    temp_Mod_arr = np.array(temp_All)[:]
+    temp_Mod_df = temp_All.copy()
+    coef_Reg = np.linalg.lstsq(temp_Mod_arr, temp_Obs, rcond=None)[0]
+    for i in range(temp_Mod_arr.shape[1]):  # ie number of variables
+        temp_Mod_arr[:, i] *= coef_Reg[i]
+        temp_Mod_df[list(temp_All)[i]] = temp_Mod_arr[:, i]
+    temp_Mod_df['temp_TOT'] = temp_Mod_df.sum(axis=1)
+    return temp_Mod_arr, temp_Mod_df
+
+
 
 # READ IN THE DATA
-df = pd.read_excel('.\data\otto_2016_gwi_excel.xlsx', sheet_name='Main',
-                   header=5, skiprows=[6])
+# df = pd.read_excel('.\data\otto_2016_gwi_excel.xlsx', sheet_name='Main',
+#                    header=5, skiprows=[6])
 
-start_yr, end_yr = 1850, 2015
+start_yr, end_yr = 1850, 2022
 start_pi, end_pi = 1860, 1879
 
 # Read Observed Temperatures
-year_Ind = df.loc[(df['Year'] >= start_yr) & (df['Year'] <= end_yr), 'Year']
+# year_Ind = df.loc[(df['Year'] >= start_yr) & (df['Year'] <= end_yr), 'Year']
 
 # ofst_Obs = df.loc[(df['Year'] >= start_pi) & (df['Year'] <= end_pi),
 #                   'Obs warm'].mean()
@@ -179,23 +186,55 @@ year_Ind = df.loc[(df['Year'] >= start_yr) & (df['Year'] <= end_yr), 'Year']
 #                   'Obs warm'] - ofst_Obs#
 
 temp_Path = './data/HadCRUT/HadCRUT.5.0.1.0.analysis.ensemble_series.global.annual.csv'
-temp_df = pd.read_csv(temp_Path).rename(columns={'Time': 'Year'})
-ofst_Obs = temp_df.loc[(temp_df['Year'] >= start_pi) &
-                       (temp_df['Year'] <= end_pi)
-                       ].mean(axis=0)
-temp_Obs_ensemble_names = list(temp_df)[3:]
-temp_Obs = temp_df.loc[(temp_df['Year'] >= start_yr) &
-                       (temp_df['Year'] <= end_yr),
-                       temp_Obs_ensemble_names] - ofst_Obs
-
+df_temp_Obs = pd.read_csv(temp_Path,
+                          ).rename(columns={'Time': 'Year'}
+                          ).set_index('Year')
+temp_Obs_ensemble_names = list(df_temp_Obs)[2:]
+ofst_Obs = df_temp_Obs.loc[(df_temp_Obs.index >= start_pi) &
+                           (df_temp_Obs.index <= end_pi),
+                           temp_Obs_ensemble_names
+                           ].mean(axis=0)
+df_temp_Obs = df_temp_Obs.loc[(df_temp_Obs.index >= start_yr) &
+                              (df_temp_Obs.index <= end_yr),
+                              temp_Obs_ensemble_names] - ofst_Obs
 # Read ERF
 forc_Path = './data/ERF Samples/'
-df_forc_Ant = pd.read_csv(forc_Path + 'rf_ant_200samples.csv', skiprows=[1])
-df_forc_Nat = pd.read_csv(forc_Path + 'rf_nat_200samples.csv', skiprows=[1])
-df_forc_Ant.rename(columns={'Unnamed: 0': 'Year'}, inplace=True)
-df_forc_Nat.rename(columns={'Unnamed: 0': 'Year'}, inplace=True)
+df_forc_Ant = pd.read_csv(forc_Path + 'rf_ant_200samples.csv', skiprows=[1]
+                          ).rename(columns={'Unnamed: 0': 'Year'}
+                          ).set_index('Year')
+df_forc_Nat = pd.read_csv(forc_Path + 'rf_nat_200samples.csv', skiprows=[1]
+                          ).rename(columns={'Unnamed: 0': 'Year'}
+                          ).set_index('Year')
+df_forc_co2 = pd.read_csv(forc_Path + 'rf_co2_200samples.csv', skiprows=[1]
+                          ).rename(columns={'Unnamed: 0': 'Year'}
+                          ).set_index('Year')
+df_forc_ch4 = pd.read_csv(forc_Path + 'rf_ch4_200samples.csv', skiprows=[1]
+                          ).rename(columns={'Unnamed: 0': 'Year'}
+                          ).set_index('Year')
+df_forc_n2o = pd.read_csv(forc_Path + 'rf_n2o_200samples.csv', skiprows=[1]
+                          ).rename(columns={'Unnamed: 0': 'Year'}
+                          ).set_index('Year')
+df_forc_other_wmghg = pd.read_csv(forc_Path + 'rf_other_wmghg_200samples.csv', skiprows=[1]
+                          ).rename(columns={'Unnamed: 0': 'Year'}
+                          ).set_index('Year')
+df_forc_Ant = df_forc_Ant.loc[df_forc_Ant.index <= end_yr]
+df_forc_Nat = df_forc_Nat.loc[df_forc_Nat.index <= end_yr]
+df_forc_co2 = df_forc_co2.loc[df_forc_co2.index <= end_yr]
+df_forc_ch4 = df_forc_ch4.loc[df_forc_ch4.index <= end_yr]
+df_forc_n2o = df_forc_n2o.loc[df_forc_n2o.index <= end_yr]
+df_forc_other_wmghg = df_forc_other_wmghg.loc[df_forc_other_wmghg.index <= end_yr]
 
-forc_All_ensemble_names = list(df_forc_Ant)[1:]
+df_forc_GHG = df_forc_co2 + df_forc_ch4 + df_forc_n2o + df_forc_other_wmghg
+df_forc_Aer = df_forc_Ant - df_forc_GHG
+
+forc_Nat_ensemble_names = list(df_forc_Ant)
+forc_Ant_ensemble_names = list(df_forc_Nat)
+if forc_Nat_ensemble_names != forc_Ant_ensemble_names:
+    print('WARNING: differing ensembles between forcing agents')
+else:
+    forc_All_ensemble_names = forc_Ant_ensemble_names
+
+
 
 # # Read Anthropogenic and Natural Forcing
 # df_forc = pd.read_excel('.\data\otto_2016_gwi_excel.xlsx', sheet_name='RF',
@@ -221,18 +260,116 @@ forc_All_ensemble_names = list(df_forc_Ant)[1:]
 #                        ['Year', 'Nat', 'Ant']]
 
 # Regress
-# for forc_Ens in forc_All_ensemble_names:
-#     for temp_Ens in temp_Obs_ensemble_names:
-#         forc_All = pd.DataFrame({'Ant': })
-        
+samples = 200
+n = samples ** 2
 
+###############################
+# t1 = dt.datetime.now()
+# i = 1
+
+# for forc_Ens in forc_All_ensemble_names[:samples]:
+#     for temp_Ens in temp_Obs_ensemble_names[:samples]:
+#         # forc_All = pd.concat([df_forc_Ant[forc_Ens], df_forc_Nat[forc_Ens]], axis=1)
+#         forc_All = pd.DataFrame({'Nat': df_forc_Nat[forc_Ens],
+#                                  'Ant': df_forc_Ant[forc_Ens]})
+#         temp_Obs = df_temp_Obs[temp_Ens]
+#         temp_Att = regress_single_gwi(forc_All, temp_Obs,
+#                                       a_params('Carbon Dioxide'),
+#                                       offset=False)
+#         # plt.plot(temp_Att_df)
+#         # Visually show how far through the calculation we are
+#         percentage = int(i/n*100)
+#         loading_bar = percentage // 5*'.' + (20 - percentage // 5)*' '
+#         print(f'calculating {loading_bar} {percentage}%', end='\r')
+#         i += 1
+# print('')
+# t2 = dt.datetime.now()
+# print(f'total caltulation took {t2-t1}')
+# # plt.show()
+
+
+################
+forc_Yrs = np.array(df_forc_Ant.index)
+temp_Yrs = np.array(df_temp_Obs.index)
+
+temp_Att_Results = np.empty((173, 2, n))
+
+
+i = 0
+t1 = dt.datetime.now()
+for forc_Ens in forc_All_ensemble_names[:samples]:
+    # Calculate forcing -> temperature response. The below steps calculate
+    # temperature for all sources simultaneously (eg Nat, GHG, and Aer)
+    forc_All = np.array([df_forc_Nat[forc_Ens], df_forc_Ant[forc_Ens]]).T
+    params = a_params('Carbon Dioxide')
+    temp_All = FTmod(forc_All.shape[0], params) @ forc_All
+    _ofst = temp_All[(forc_Yrs >= start_pi) & (forc_Yrs <= end_pi), :].mean(axis=0)
+    temp_Mod = temp_All[(forc_Yrs >= start_yr) & (forc_Yrs <= end_yr)] - _ofst
+
+    # Decide whether to include a constant offset term in regression
+    offset = False
+    if offset:
+        temp_Mod = np.append(temp_Mod, np.ones((temp_Mod.shape[0], 1)), axis=1)
+
+    for temp_Ens in temp_Obs_ensemble_names[:samples]:
+        # Select the relevant observational data
+        temp_Obs = np.array(df_temp_Obs[temp_Ens])
+
+        # Carry out regression calculation
+
+        coef_Reg = np.linalg.lstsq(temp_Mod, temp_Obs, rcond=None)[0]
+        temp_Att = temp_Mod * coef_Reg
+        temp_Att_Results[:, :, i] = temp_Att
+        # Visual display
+        percentage = int((i+1)/n*100)
+        loading_bar = percentage // 5*'.' + (20 - percentage // 5)*' '
+        print(f'calculating {loading_bar} {percentage}%', end='\r')
+        i += 1
+
+t2 = dt.datetime.now()
+print(f'Total calculation took {t2-t1}')
+err_pos = (df_temp_Obs.quantile(q=0.95, axis=1) -
+           df_temp_Obs.quantile(q=0.5, axis=1))
+err_neg = (df_temp_Obs.quantile(q=0.5, axis=1) -
+           df_temp_Obs.quantile(q=0.05, axis=1))
+plt.errorbar(temp_Yrs, df_temp_Obs.quantile(q=0.5, axis=1),
+             yerr=(err_neg, err_pos), fmt='o',
+             color='black')
+# plt.fill_between(temp_Yrs,
+#                  df_temp_Obs.quantile(q=0.05, axis=1),
+#                  df_temp_Obs.quantile(q=0.95, axis=1),
+#                  color='black', alpha=0.05)
+
+vars = ['Nat', 'Ant']
+var_cols = ['blue', 'orange']
+for i in range(len(vars)):
+    plt.fill_between(temp_Yrs,  
+                     np.percentile(temp_Att_Results[:, i, :], (5), axis=1),
+                     np.percentile(temp_Att_Results[:, i, :], (95), axis=1),
+                     color=var_cols[i], label=vars[i], alpha=0.5)
+    plt.plot(temp_Yrs, np.percentile(temp_Att_Results[:, i, :], (50), axis=1),
+             color=var_cols[i])
+
+temp_TOT_Results = temp_Att_Results.sum(axis=1)
+plt.fill_between(temp_Yrs,  
+                 np.percentile(temp_TOT_Results[:, :], (5), axis=1),
+                 np.percentile(temp_TOT_Results[:, :], (95), axis=1),
+                 color='red', alpha=0.5)
+plt.plot(temp_Yrs, np.percentile(temp_TOT_Results[:, :], (50), axis=1),
+         color='red')
+
+plt.show()
+
+
+# plt.show()
+
+        
 # ERFs are of shape (variable, ensemble, timeseries) dimension:
 # * variable is variable (Nat, Ant, ...)
 # * ensemble is ensemble (1, 2, 3, ...)
 # * timeseries is time dimension, ie the actual timeseries
 # Note that i = j
 
-# temp_Att = regress_single_gwi(forc_All, temp_Obs, a_params('Carbon Dioxide'))
 
 # # PLOT DATA
 # plt.scatter(year_Ind, temp_Obs)
